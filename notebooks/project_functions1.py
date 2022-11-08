a8608516a7a172cb0153c379c50f710c24b84b63
VisheshThapa <visheshjthapa@gmail.com>
2022-11-07 18:32:51 -0800

    V part

---
+++ notebooks/project_functions1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+def load_and_process(url_or_path_to_csv_file):
+    df1 = (pd.read_csv(url_or_path_to_csv_file)
+      .pipe(lambda x: x.replace({'\$': '', ',': '', '%': '', '\(':'', '\)':'',}, regex=True))
+      .replace('-', np.nan).dropna(subset=['revenues', 'revenue_percent_change', 'profits','profits_percent_change', 'assets', 'market_value',     'employees']))
+    df2 = (df1.assign(revenue_over_profit = lambda x: x['revenues'].astype(float)/x['profits'].astype(float))
+      .assign(market_over_asset = lambda x: x['market_value'].astype(float)/x['assets'].astype(float)))
+    return df2
